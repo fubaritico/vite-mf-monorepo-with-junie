5743844a4be33b2074cbbdb3a81784b810d21b77
Stéphane Cottereau <scottereau@greenflex.com>
2025-04-21 19:39:57 +0200

    feat(commitlint): add commitlint for conventional commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
@@ -15,15 +15,19 @@ module.exports = {
         'refactor',
         'revert',
         'style',
-        'test'
-      ]
+        'test',
+      ],
     ],
-    'type-case': [2, 'always', 'lower'],
+    'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
-    'scope-case': [2, 'always', 'lower'],
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 100]
-  }
-};
+    'header-max-length': [2, 'always', 100],
+  },
+}
